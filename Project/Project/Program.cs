9d4353c36a4a2cfd4a19755e8c092ca62f76038d
FatbardhKadriu <44509862+FatbardhKadriu@users.noreply.github.com>
2019-04-21 16:35:14 +0200

    Test

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -3,14 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Project
-{
+namespace BookCipher
+{   
     class Program
     {
+        //Fajlli
+        static readonly string textFile = @"C:\Users\Bardhi\Desktop\libri\bardhi.txt";
+
+        public static string readFile(string textFile)
+        {
+            if(File.Exists(textFile))
+            {
+                string txt = File.ReadAllText(textFile);
+                return txt;
+            }
+            return "Fajlli nuk ekziston\n";
+        }
+        static string Encrypt(string plaintext, string book)
+        {
+            string[] wordArray = book.Split(' ');
+            string[] plainTextArray = plaintext.Split(' ');
+            string cipherText = "";
+            for (int i = 0; i < plainTextArray.Length; i++)
+            {
+                string word = plainTextArray[i];
+                int index = Array.IndexOf(wordArray, word);
+                if (index < 0)
+                {
+                    throw new Exception();
+                }
+
+                cipherText += index + 1;
+                cipherText += " ";
+            }
+
+            return cipherText.TrimEnd();
+        }
+        static string Decrypt(string ciphertext, string book)
+        {
+            int[] indexes = ciphertext
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => int.Parse(n) - 1)
+                .ToArray();
+
+            string plaintext = "";
+            string[] wordArray = book.Split(' ');
+            foreach (int index in indexes)
+            {
+                plaintext += wordArray[index] + " ";
+            }
+
+            return plaintext.TrimEnd();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            String plaintext = "This is a book";
+            String ciphertext = Encrypt(plaintext, readFile(textFile));
+            String plaintextAgain = Decrypt(ciphertext, readFile(textFile));
+            Console.WriteLine("Clear Text : " + plaintext);
+            Console.WriteLine("CipherText : " + ciphertext);
+            Console.WriteLine("Clear Text Again : " + plaintextAgain); 
         }
     }
 }
